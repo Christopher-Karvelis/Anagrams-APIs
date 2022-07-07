95ef01fae3dcbeb1755c325073894cd15f363dc4
Christoforos Karvelis <christoforos.karvelis@gmail.com>
2022-07-08 01:16:45 +0300

    Added endpointC

--- routes/anagrams.js
+++ routes/anagrams.js
@@ -27,8 +27,8 @@ function areAnagrams(string1, string2) {
 
 function getAnagrams(word, sentence) {
     var anagrams = [];
-    const uniqueAnagrams = new Set();
-    var words = sentence.split(' ');
+    const uniqueAnagrams = new Set();   // Set with O(1) look up to check for unique anagrams in the sentence
+    var words = sentence.split(' ');    // Break the string in to an array of words
     var j = 0;
     for (let i = 0; i < words.length; i++) {
         if(areAnagrams(word, words[i])){
@@ -42,11 +42,44 @@ function getAnagrams(word, sentence) {
     return anagrams;
 }
 
+function getAnagramGroups(sentence) {
+    var groupAnagrams = [];
+    var words = sentence.split(' ');    // Break the string in to an array of words
+    var w = 0;
+    for (let i = 0; i < words.length; i++) {
+        const uniqueAnagrams = new Set();   // Set with O(1) look up to check for unique anagrams in the sentence
+        const firstWord = words[i];
+        const group = [];
+        var g = 0;
+        for (let j = i+1; j < words.length; j++) {
+            if (areAnagrams(words[i], words[j])) {
+                if (!uniqueAnagrams.has(words[j])) {
+                    uniqueAnagrams.add(words[j]);
+                    group[g] = words[j];
+                    g++;
+                }
+            }
+        }
+        if (uniqueAnagrams.size != 0) {
+            if (!uniqueAnagrams.has(firstWord)) {
+                group[g] = firstWord;
+            }
+            
+            if(group.length > 1) {
+                groupAnagrams[w] = group;
+                w++;
+            }
+        }
+    }
+    return groupAnagrams;
+}
+
 // Default endpoint
 router.get('/', function(req, res) {
     res.send("Welcome to anagrams!")
 })
 
+// Post endpoint A
 router.post('/endpointA', function(req, res) {
     const { string1, string2 } = req.body;
     console.log(string1, string2);
@@ -55,7 +88,7 @@ router.post('/endpointA', function(req, res) {
         outcome : outcome
     })
 })
-
+// Post endpoint B
 router.post('/endpointB', function(req, res) {
     const { word, sentence } = req.body;
     console.log(word, sentence);
@@ -64,6 +97,14 @@ router.post('/endpointB', function(req, res) {
         outcome : outcome
     })
 })
-
+// Post endpoint C
+router.post('/endpointC', function(req, res) {
+    const { sentence } = req.body;
+    console.log(sentence);
+    var outcome = getAnagramGroups(sentence);
+    res.json({
+        outcome : outcome
+    })
+})
 // Export so it can be used in index.js
 export default router
