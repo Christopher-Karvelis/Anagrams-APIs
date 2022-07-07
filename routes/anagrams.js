f9c8550c3b5e77d7eba96f80c87724e249a5067f
Christoforos Karvelis <christoforos.karvelis@gmail.com>
2022-07-07 19:28:19 +0300

    Added endpointB

--- routes/anagrams.js
+++ routes/anagrams.js
@@ -4,26 +4,43 @@ const router = express.Router()
 
 function areAnagrams(string1, string2) {
     const asciiCount = new Array(256).fill(0);
+
     if(string1.length != string2.length){
         return false;
     }
-    // Iterate through strings and add 1 to the coresponding ASCI character for String1 and add -1 for String2 
+    // Convert both strings to lowercase as ASCII code for 'l' is different from 'L'
+    string1 = string1.toLowerCase();
+    string2 = string2.toLowerCase();
+    // Iterate through strings adding 1 to the coresponding ASCI character for String1 while substracting 1 for String2 
     for (let i = 0; i < string1.length; i++) {
-        // convert both strings to lowercase as ASCII code for 'l' is different from 'L'
-        string1 = string1.toLowerCase();
-        string2 = string2.toLowerCase();
         asciiCount[string1.charAt(i).charCodeAt(0)]+= 1;
         asciiCount[string2.charAt(i).charCodeAt(0)]-= 1;
     }
     // See if we encountered the same characters
     for (let i = 0; i < asciiCount.length; i++) {
-        if (asciiCount[i] != 0) { // Each ascii count should be zero 
-            console.log(asciiCount[i]);
+        if (asciiCount[i] != 0) { // Each ascii count should be zero if string1 is anagram of string2
             return false;
         }
     }
     return true;
-  }
+}
+
+function getAnagrams(word, sentence) {
+    var anagrams = [];
+    const uniqueAnagrams = new Set();
+    var words = sentence.split(' ');
+    var j = 0;
+    for (let i = 0; i < words.length; i++) {
+        if(areAnagrams(word, words[i])){
+            if(!uniqueAnagrams.has(words[i])) {
+                uniqueAnagrams.add(words[i]);
+                anagrams[j] = words[i];
+                j++;
+            }
+        }
+    }
+    return anagrams;
+}
 
 // Default endpoint
 router.get('/', function(req, res) {
@@ -31,10 +48,18 @@ router.get('/', function(req, res) {
 })
 
 router.post('/endpointA', function(req, res) {
-    const { String1, String2 } = req.body;
-    console.log(String1, String2);
-    let outcome = areAnagrams(String1, String2);
+    const { string1, string2 } = req.body;
+    console.log(string1, string2);
+    var outcome = areAnagrams(string1, string2);
+    res.json({
+        outcome : outcome
+    })
+})
 
+router.post('/endpointB', function(req, res) {
+    const { word, sentence } = req.body;
+    console.log(word, sentence);
+    var outcome = getAnagrams(word, sentence);
     res.json({
         outcome : outcome
     })
