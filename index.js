85e2c8426191f2d2a095014405b51ee12aa159fc
Christoforos Karvelis <christoforos.karvelis@gmail.com>
2022-07-11 21:29:44 +0300

    Deployed on Heroku

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import bodyParser from "body-parser";
 import anagramsRouter from "./routes/endpoints.js";
 
 const app = express();
-
+const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 
 app.use('/', anagramsRouter);
 
-app.listen(8080, function() {
-    console.log("Server running on port 8080");
+app.listen(port, function() {
+    console.log("Server running on port " + port);
 });
